11686b84a16b3ffb61cfdd54c53be30f07de8b7e
Alex Erohin <alexanderErohin@yandex.ru>
2023-11-09 16:06:35 +0300

    init commit

---
+++ tplinkrouterc6u/dataclass.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass, field
+from tplinkrouterc6u.enum import Wifi
+
+
+@dataclass
+class Firmware:
+    def __init__(self, hardware: str, model: str, firmware: str) -> None:
+        self.hardware_version = hardware
+        self.model = model
+        self.firmware_version = firmware
+
+
+@dataclass
+class Device:
+    def __init__(self, type: Wifi, macaddr: str, ipaddr: str, hostname: str) -> None:
+        self.type = type
+        self.macaddr = macaddr
+        self.ipaddr = ipaddr
+        self.hostname = hostname
+
+
+@dataclass
+class Status:
+    macaddr: str
+    wired_total: int
+    wifi_clients_total: int
+    guest_clients_total: int
+    clients_total: int
+    guest_2g_enable: bool
+    guest_5g_enable: bool
+    wifi_2g_enable: bool
+    wifi_5g_enable: bool
+    devices: list[Device]
---
+++ tplinkrouterc6u/enum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Wifi(Enum):
+    WIFI_2G = 'wireless_2g'
+    WIFI_5G = 'wireless_5g'
+    WIFI_GUEST_2G = 'guest_2g'
+    WIFI_GUEST_5G = 'guest_5g'
