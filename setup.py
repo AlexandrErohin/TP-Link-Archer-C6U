20c542be3a034124e90393624063f5d196649669
Alex Erohin <alexanderErohin@yandex.ru>
2023-11-22 11:36:01 +0300

    update README.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tplinkrouterc6u",
-    version="0.1.1",
+    version="0.1.2",
     author="Alex Erohin",
     author_email="alexanderErohin@yandex.ru",
     description="TP-Link Router API for Archer C6U and Archer AX10",
