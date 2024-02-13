e72ece57f9633c4d89ed66e5580c872a2e8845b8
Alex Erohin <alexanderErohin@yandex.ru>
2024-02-13 10:17:16 +0300

    added AbstractRouter to __init__.py

--- tplinkrouterc6u/__init__.py
+++ tplinkrouterc6u/__init__.py
@@ -1,3 +1,3 @@
-from tplinkrouterc6u.client import TplinkRouter, TplinkRouterProvider, TplinkC1200Router
+from tplinkrouterc6u.client import TplinkRouter, TplinkRouterProvider, TplinkC1200Router, AbstractRouter
 from tplinkrouterc6u.enum import Wifi
 from tplinkrouterc6u.dataclass import Firmware, Status, Device, IPv4Reservation, IPv4DHCPLease, IPv4Status
