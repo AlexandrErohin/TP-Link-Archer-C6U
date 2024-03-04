cf3e86de3d7845f52b75de50b1c7a7742e774b54
Gordon Aspin <gordon.aspin@gmail.com>
2024-03-04 03:11:34 -0500

    Support for Wifi 6G (#16)
    Support for Wifi 6G

--- tplinkrouterc6u/enum.py
+++ tplinkrouterc6u/enum.py
@@ -4,7 +4,11 @@ from enum import Enum
 class Wifi(Enum):
     WIFI_2G = 'wireless_2g'
     WIFI_5G = 'wireless_5g'
+    WIFI_6G = 'wireless_6g'
     WIFI_GUEST_2G = 'guest_2g'
     WIFI_GUEST_5G = 'guest_5g'
+    WIFI_GUEST_6G = 'guest_6g'
     WIFI_IOT_2G = 'iot_2g'
     WIFI_IOT_5G = 'iot_5g'
+    WIFI_IOT_6G = 'iot_6g'
+    WIFI_UNKNOWN = 'unknown'
