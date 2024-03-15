14b252f00f251325c3323bbb636a5db16e73315e
Alex Erohin <alexanderErohin@yandex.ru>
2024-03-15 16:02:00 +0300

    added github tests and fixed README.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tplinkrouterc6u",
-    version="3.3.0",
+    version="3.3.1",
     author="Alex Erohin",
     author_email="alexanderErohin@yandex.ru",
     description="TP-Link Router API",
@@ -14,9 +14,13 @@ setuptools.setup(
     url="https://github.com/AlexandrErohin/TP-Link-Archer-C6U",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     install_requires=['requests', 'pycryptodome', 'macaddress'],
     python_requires='>=3.10',
