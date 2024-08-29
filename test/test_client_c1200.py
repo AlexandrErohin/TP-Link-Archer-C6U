e3d624302c3c7bdc28f77a0c8f26544343fa2826
swwgames <95527278+swwgames@users.noreply.github.com>
2024-08-29 16:28:34 +0200

    added a test for the set_wifi function

---
+++ test/test_client_c1200.py
@@ -0,0 +1,141 @@
+import unittest
+import json
+from tplinkrouterc6u import (
+    TplinkC1200Router,
+    Connection,
+    ClientException
+)
+
+
+class TestTPLinkC1200Client(unittest.TestCase):
+
+    def test_set_led_on(self) -> None:
+
+        response_led_general_read = '''
+        {
+            "enable": "off",
+            "time_set": "yes",
+            "ledpm_support": "yes"
+        }
+        '''
+
+        response_led_general_write = '''
+        {
+            "enable": "on",
+            "time_set": "yes",
+            "ledpm_support": "yes"
+        }
+        '''
+
+        class TPLinkRouterTest(TplinkC1200Router):
+            def request(self, path: str, data: str,
+                        ignore_response: bool = False, ignore_errors: bool = False) -> dict | None:
+                if path == 'admin/ledgeneral?form=setting&operation=read':
+                    return json.loads(response_led_general_read)
+                if path == 'admin/ledgeneral?form=setting&operation=write':
+                    self.captured_path = path
+                    return json.loads(response_led_general_write)
+                raise ClientException()
+
+        client = TPLinkRouterTest('', '')
+        
+        client.set_led(True)
+
+        expected_path = "admin/ledgeneral?form=setting&operation=write"
+        
+        self.assertEqual(client.captured_path, expected_path)
+
+    def test_set_led_off(self) -> None:
+
+        response_led_general_read = '''
+        {
+            "enable": "on",
+            "time_set": "yes",
+            "ledpm_support": "yes"
+        }
+        '''
+
+        response_led_general_write = '''
+        {
+            "enable": "off",
+            "time_set": "yes",
+            "ledpm_support": "yes"
+        }
+        '''
+
+        class TPLinkRouterTest(TplinkC1200Router):
+            def request(self, path: str, data: str,
+                        ignore_response: bool = False, ignore_errors: bool = False) -> dict | None:
+                if path == 'admin/ledgeneral?form=setting&operation=read':
+                    return json.loads(response_led_general_read)
+                elif path == 'admin/ledgeneral?form=setting&operation=write':
+                    self.captured_path = path
+                    return json.loads(response_led_general_write)
+                raise ClientException()
+
+        client = TPLinkRouterTest('', '')
+
+        client.set_led(False)
+
+        expected_path = "admin/ledgeneral?form=setting&operation=write"
+        
+        self.assertEqual(client.captured_path, expected_path)
+
+    def test_led_status(self) -> None:
+
+        response_led_general_read = '''
+        {
+            "enable": "on",
+            "time_set": "yes",
+            "ledpm_support": "yes"
+        }
+        '''
+        
+        class TPLinkRouterTest(TplinkC1200Router):
+            def request(self, path: str, data: str,
+                        ignore_response: bool = False, ignore_errors: bool = False) -> dict | None:
+                if path == 'admin/ledgeneral?form=setting&operation=read':
+                    return json.loads(response_led_general_read)
+                raise ClientException()
+
+        client = TPLinkRouterTest('', '')
+
+        led_status = client.get_led()
+        self.assertTrue(led_status)
+
+    def test_set_wifi(self) -> None:
+
+        class TPLinkRouterTest(TplinkC1200Router):
+            def request(self, path: str, data: str,
+                        ignore_response: bool = False, ignore_errors: bool = False) -> dict | None:
+
+                self.captured_path = path
+                self.captured_data = data
+
+        client = TPLinkRouterTest('', '')
+        client.set_wifi(
+            Connection.HOST_5G,
+            enable=True,
+            ssid="TestSSID",
+            hidden="no",
+            encryption="WPA3-PSK",
+            psk_version="2",
+            psk_cipher="AES",
+            psk_key="testkey123",
+            hwmode="11ac",
+            htmode="VHT20",
+            channel=36,
+            txpower="20",
+            disabled_all="no"
+        )
+        
+        expected_data = ("operation=write&enable=on&ssid=TestSSID&hidden=no&encryption=WPA3-PSK&"
+                         "psk_version=2&psk_cipher=AES&psk_key=testkey123&hwmode=11ac&"
+                         "htmode=VHT20&channel=36&txpower=20&disabled_all=no")
+        expected_path = f"admin/wireless?form=wireless_5g&{expected_data}"
+        
+        self.assertEqual(client.captured_path, expected_path)
+        self.assertEqual(client.captured_data, expected_data)
+
+if __name__ == '__main__':
+    unittest.main()
