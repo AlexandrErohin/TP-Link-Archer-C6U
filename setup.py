be2cd979965a36bc19315fe0a884d7e2317ef4a7
Alex Erohin <alexanderErohin@yandex.ru>
2024-11-21 14:35:33 +0300

    added NX510v v1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tplinkrouterc6u",
-    version="5.3.0",
+    version="5.4.0",
     author="Alex Erohin",
     author_email="alexanderErohin@yandex.ru",
     description="TP-Link Router API",
