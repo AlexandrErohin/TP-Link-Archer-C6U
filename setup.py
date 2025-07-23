c29aee62a1f97a469612218d8107ebaade01bc34
Alex Erohin <alexanderErohin@yandex.ru>
2025-07-23 11:36:50 +0300

    added supported routers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tplinkrouterc6u",
-    version="5.9.2",
+    version="5.9.3",
     author="Alex Erohin",
     author_email="alexanderErohin@yandex.ru",
     description="TP-Link Router API (supports also Mercusys Router)",
