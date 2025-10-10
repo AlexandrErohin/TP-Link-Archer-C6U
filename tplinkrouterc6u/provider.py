1a28958283d2d8db20e621e181bd434166f136c9
Niels <niels.muendler@inf.ethz.ch>
2025-10-10 09:23:55 +0200

    Fix

--- tplinkrouterc6u/provider.py
+++ tplinkrouterc6u/provider.py
@@ -16,13 +16,14 @@ from tplinkrouterc6u.client.wdr import TplinkWDRRouter
 
 class TplinkRouterProvider:
     @staticmethod
-    def get_client(host: str, password: str, username: str | None = None, logger: Logger = None,
+    def get_client(host: str, password: str, username: str = "admin", logger: Logger = None,
                    verify_ssl: bool = True, timeout: int = 30) -> AbstractRouter:
-        if username is None:
-            router = TplinkRe700XRouter(
+        if not username or username == "admin":
+            # Try Re700X first as it has no username
+            client = TplinkRe700XRouter(
                 host=host, password=password, logger=logger, verify_ssl=verify_ssl, timeout=timeout)
-            if router.supports():
-                return router
+            if client.supports():
+                return client
         for client in [TplinkC5400XRouter, TPLinkVRClient, TPLinkEXClient, TPLinkMRClient, TPLinkDecoClient,
                        TPLinkXDRClient, TplinkRouter, TplinkC80Router, TplinkWDRRouter]:
             router = client(host, password, username, logger, verify_ssl, timeout)
