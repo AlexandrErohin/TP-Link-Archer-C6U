82bc31abe1d3cb8a1540872f5f2f4f7f4160e463
Alex Erohin <alexanderErohin@yandex.ru>
2025-10-24 09:04:52 +0300

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tplinkrouterc6u",
-    version="5.10.1",
+    version="5.10.2",
     author="Alex Erohin",
     author_email="alexanderErohin@yandex.ru",
     description="TP-Link Router API (supports also Mercusys Router)",
