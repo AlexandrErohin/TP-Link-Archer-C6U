903035885ce38a89a5acf689a624431dae3f42be
Alex Erohin <alexanderErohin@yandex.ru>
2026-01-23 14:57:39 +0300

    fix TPLinkMR6400v7Client

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tplinkrouterc6u",
-    version="5.13.0",
+    version="5.14.0",
     author="Alex Erohin",
     author_email="alexanderErohin@yandex.ru",
     description="TP-Link Router API (supports also Mercusys Router)",
