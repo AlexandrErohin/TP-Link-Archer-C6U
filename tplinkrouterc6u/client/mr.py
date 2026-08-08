eff315c11a2b2d2d5144957e92e1cfdaeaadb44e
david9600 <david.c.arnold@icloud.com>
2026-08-08 16:03:02 +1000

    Add IPv6 in get_status in mr.py

--- tplinkrouterc6u/client/mr.py
+++ tplinkrouterc6u/client/mr.py
@@ -169,6 +169,8 @@ class TPLinkMRClientBase(AbstractRouter):
                 'X_TP_TotalPacketsSent',
                 'X_TP_TotalPacketsReceived',
             ]),
+            self.ActItem(self.ActItem.GS, 'WAN_IP_CONN',
+                         attrs=['enable', 'X_TP_IPv6Enabled', 'X_TP_IPv6ConnStatus', 'X_TP_ExternalIPv6Address']),
         ]
         _, values = self.req_act(acts)
 
@@ -226,6 +228,13 @@ class TPLinkMRClientBase(AbstractRouter):
                     '')
             devices[val['associatedDeviceMACAddress']].packets_sent = int(val['X_TP_TotalPacketsSent'])
             devices[val['associatedDeviceMACAddress']].packets_received = int(val['X_TP_TotalPacketsReceived'])
+
+        for item in self._to_list(values.get('6')):
+            if int(item['enable']) == 0 and values.get('6').__class__ == list:
+                continue
+            status.wan_ipv6_enabled = item.get('X_TP_IPv6Enabled') == '1'
+            status._wan_ipv6_conn_status = item.get('X_TP_IPv6ConnStatus', '')
+            status._wan_ipv6_addr = get_ipv6(item.get('X_TP_ExternalIPv6Address', '::'))
 
         try:
             stat_acts = [self.ActItem(self.ActItem.GL, 'STAT_ENTRY')]
