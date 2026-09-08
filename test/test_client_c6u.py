1a0a476daa1cb4d50887f7cbc31945e46ef10d26
Alex Erohin <alexanderErohin@yandex.ru>
2026-09-08 17:00:02 +0300

    fix test

--- test/test_client_c6u.py
+++ test/test_client_c6u.py
@@ -1365,7 +1365,7 @@ class TestTPLinkClient(TestCase):
         client = TPLinkRouterTest('', '')
         result = client.set_ipv4_dhcps(False)
         self.assertIsNone(result)
-        self.assertEqual(check_url, 'admin/dhcps?form=setting')
+        self.assertEqual(check_url, client._url_ipv4_dhcps)
         body = dict(parse_qsl(check_data, keep_blank_values=True))
         self.assertEqual(body['operation'], 'write')
         self.assertEqual(body['enable'], 'off')
